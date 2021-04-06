8000d989e12900121566a567b76c1d361e040007
yedukondalu7479 <yedukondalub@outlook.com>
2021-04-06 16:58:36 +0530

    chatapp

--- redisapp/app.js
+++ redisapp/app.js
@@ -1,152 +1,33 @@
 const express = require('express');
+const axios = require('axios');
+const redis = require('redis');
 const app = express();
-const port = process.env.PORT || 9080;
-const mongo = require('mongodb');
-const MongoClient = mongo.MongoClient;
-const bodParser = require('body-parser');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongourl = "mongodb://localhost:27017";
-let db;
-let col_name = "user";
-app.use(cors());
-///parse data for post call
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodParser.json())
-app.use(express.static(__dirname+'/public'));
-app.set('views','./views');
-app.set('view engine','ejs');
+const port = 9870;
 
-// Health Check
-app.get('/health',(req,res) => {
-    res.status(200).send("Health Ok")
+const client = redis.createClient({
+    host:'localhost',
+    port:6379
 })
 
-// Home page
-app.get('/',(req,res) => {
-    db.collection(col_name).find().toArray((err,result) => {
-        if(err) throw err;
-       res.render('index',{data:result})
-    })
-})
 
-// Add user
-app.get('/new',(req,res) => {
-    res.render('admin')
-})
-
-// post User
-app.post('/addUser',(req,res) => {
-    console.log(req.body)
-    var data ={
-        "name": req.body.name,
-        "email": req.body.email,
-        "city": req.body.city,
-        "role": req.body.role?req.body.role:'User',
-        "isActive": req.body.isActive?req.body.isActive:true
-    }
-    db.collection(col_name).insert(data,(err,result) => {
-        if(err) throw err;
-        res.redirect('/')
-    })
-})
-
-// getAllUser
-app.get('/users',(req,res) => {
-    var query = {}
-    if(req.query.city && req.query.role){
-        query = {city:req.query.city,isActive:true,role:req.query.role}
-    }
-    else if(req.query.city){
-        query = {city:req.query.city,isActive:true}
-    }else if(req.query.role){
-        query = {role:req.query.role,isActive:true}
-    }
-    db.collection(col_name).find(query).toArray((err,result) => {
-        if(err) throw err;
-        res.status(200).send(result)
-    })
-})
-
-// get Particular Use
-app.get('/user/:id',(req,res) => {
-    var id = mongo.ObjectID(req.params.id)
-    var query = {};
-    query = {_id:id}
-    db.collection(col_name).findOne(query,(err,result)=> {
-        if(err) throw err;
-        res.send(result)
-    })
-})
-
-// update user
-app.put('/updateUser',(req,res) => {
-    let id = req.body._id
-    db.collection(col_name).update(
-        {_id:mongo.ObjectID(id)},
-        {
-            $set:{
-                name: req.body.name,
-                email: req.body.email,
-                city: req.body.city,
-                role: req.body.role,
-                isActive: req.body.isActive?req.body.isActive:true
-            }
-        },(err,result) => {
-            if(err) throw err;
-            res.send('Data Updated')
+app.get('/data',(req,res) => {
+    const userinput = req.query.country?req.query.country:'India';
+    const url = `https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&page=${userinput}`;
+    return client.get(`country:${userinput}`,(err,result) => {
+        if(result){
+            const output = JSON.parse(result);
+            return res.send(output)
+        }else{
+            return axios.get(url)
+                .then(response => {
+                    const output = response.data;
+                    client.setex(`country:${userinput}`,3600,JSON.stringify({source:'Redis',output}));
+                    return res.send({source:'api',output})
+                })
         }
-    )
-})
-
-//delete(hard Delete)
-app.delete('/deleteUser',(req,res) => {
-    let Id = mongo.ObjectID(req.body._id);
-    db.collection(col_name).remove({_id:Id},(err,result) => {
-        if(err) throw err;
-        res.send("Data Deleted")
     })
 })
 
-//activate user
-app.put('/activateUser',(req,res) => {
-    let id = req.body._id
-    db.collection(col_name).update(
-        {_id:mongo.ObjectID(id)},
-        {
-            $set:{
-                isActive: true
-            }
-        },(err,result) => {
-            if(err) throw err;
-            res.send('User Activated')
-        }
-    )
-})
-
-//deactivate user (soft Delete)
-app.put('/deactivateUser',(req,res) => {
-    let id = req.body._id
-    db.collection(col_name).update(
-        {_id:mongo.ObjectID(id)},
-        {
-            $set:{
-                isActive: false
-            }
-        },(err,result) => {
-            if(err) throw err;
-            res.send('User Deactivated')
-        }
-    )
-})
-
-MongoClient.connect(mongourl,(err,connection)=>{
-    if(err) console.log(err);
-    db = connection.db('marchnode');
-})
-
-//start server
-app.listen(port,(err) => {
-    if(err) throw err;
+app.listen(port,(err)=>{
     console.log(`Server is running on port ${port}`)
 })
